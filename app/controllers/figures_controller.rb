05ec863157e4e4f125137176ffacd1c5c1c94931
github username <github email address>
2016-12-20 15:40:51 -0500

    done

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,40 +1,68 @@
 class FiguresController < ApplicationController
 
   get '/figures' do 
-    @figures = figure.all
+    @figures = Figure.all
     erb :'/figures/index'
   end
 
 
 
   get '/figures/new' do
+    @landmarks = Landmark.all
+    @titles = Title.all
     erb :'/figures/new'
   end
 
   get '/figures/:id/edit' do
-    @figure = figure.find(params[:id])
+    @figure = Figure.find(params[:id])
+    @landmarks = Landmark.all
+    @titles = Title.all 
     erb :'/figures/edit'
   end
 
   post '/figures' do
-    @figure = figure.create(params[:figure])
+    @figure = Figure.create(params[:figure])
+    #binding.pry
+    if !params[:landmark][:name].empty?
+      l = Landmark.create(params[:landmark])
+      @figure.landmarks << l
+      @figure.save
+    end
+    if !params[:title][:name].empty?
+      t = Title.create(params[:title])
+      @figure.titles << t
+      @figure.save
+    end
+    #binding.pry
     redirect to "/figures/#{@figure.id}"
   end
 
   delete '/figures/:id/delete' do
-    @figure = figure.find(params[:id])
+    @figure = Figure.find(params[:id])
     @figure.destroy
     redirect to '/figures'
   end
 
   patch '/figures/:id' do
-    @figure = figure.find(params[:id])
+    @figure = Figure.find(params[:id])
     @figure.update(params[:figure])
+    if !params[:landmark][:name].empty?
+      l = Landmark.create(params[:landmark])
+      @figure.landmarks << l
+      @figure.save
+    end
+    if !params[:title][:name].empty?
+      t = Title.create(params[:title])
+      @figure.titles << t
+      @figure.save
+    end
+   
+
     redirect to "/figures/#{@figure.id}"
   end
 
-    get '/figures/:id' do 
-    @figure = figure.find(params[:id])
+  get '/figures/:id' do 
+    @figure = Figure.find(params[:id])
     erb :'/figures/show'
   end
 
